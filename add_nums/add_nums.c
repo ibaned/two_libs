cb412a66108ee612d306a702f3d71bed28912273
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-07-13 14:26:24 -0400

    demo header-only library

--- add_nums/add_nums.c
+++ add_nums/add_nums.c
@@ -1,5 +1,8 @@
 #include "add_nums.h"
+#include <incr_num.h>
 
 int add_nums(int a, int b) {
-  return a + b;
+  for (int i = 0; i < b; ++i)
+    a = incr_num(a);
+  return a;
 }
---
+++ incr_num/incr_num.h
@@ -0,0 +1,6 @@
+#ifndef INCR_NUM_H
+#define INCR_NUM_H
+
+inline static int incr_num(int x) { return x + 1; }
+
+#endif
